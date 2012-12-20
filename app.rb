bdf8c6737e15819e2306bca95efe7cd6b9d699ca
Glass_saga <glass.saga@gmail.com>
2012-12-20 16:58:01 +0900

    helpers in ext file

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
-require "sinatra/base"
-require "sinatra/namespace"
 require "json"
 require "haml"
+require "sinatra/base"
+require "sinatra/namespace"
 require "leofs_manager_client"
+require_relative "lib/helpers"
 
 class LeoTamer < Sinatra::Base
   VERSION = "0.2.2"
@@ -43,45 +44,7 @@ class LeoTamer < Sinatra::Base
     end
   end
 
-  helpers do
-    def debug(str)
-      puts str if $DEBUG
-    end
-
-    def required_params(*params_to_check)
-      request_params = params
-      noexist_params = params_to_check.reject {|param| request_params[param] }
-      unless noexist_params.empty?
-        noexist_params.map! {|param| "'#{param}'" }
-        raise "parameter #{noexist_params.join(" ")} are required"
-      end
-      if params_to_check.size == 1
-        request_params[params_to_check.first]
-      else
-        params_to_check.map {|param| request_params[param] }
-      end
-    end
-
-    def required_sessions(*keys_to_check)
-      valid = keys_to_check.all? {|key| session.has_key?(key) }
-      raise "invalid session" unless valid
-      if keys_to_check.size == 1
-        session[keys_to_check.first]
-      else
-        keys_to_check.map {|key| session[key] }
-      end
-    end
-
-    # error msg for Ext.form
-    def json_err_msg(msg)
-      { 
-        success: false,
-        errors: {
-          reason: msg
-        }
-      }.to_json
-    end
-  end
+  helpers TamerHelpers
 
   error do
     ex = env['sinatra.error'] # Exception
