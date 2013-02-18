c5883df387a8c8c04f73bb22581bde7ddbdb5794
Glass_saga <glass.saga@gmail.com>
2013-02-18 09:21:05 +0000

    add growthforecast.rb

---
+++ lib/growthforecast.rb
@@ -0,0 +1,37 @@
+require "open-uri"
+require "json"
+
+module GrowthForecast
+  class Client
+    def initialize(host, port)
+      @host = host.freeze
+      @port = port
+      @base_uri = "http://#{@host}:#{@port}/".freeze
+    end
+
+    attr_reader :host, :port
+
+    def export(service_name, section_name, graph, opt=nil)
+      path = "xport/#{service_name}/#{section_name}/#{graph_name}"
+      if opt && opt.is_a?(Hash)
+        path.concat("?")
+        param_str = opt.map {|k, v| "#{k}=#{v}" }.join("&")
+        path.cocnat(param_str)
+      end
+      return request(path)
+    end
+
+    private
+
+    JSON_OPTS = {
+      symbolize_names: true
+    }
+
+    def request(path)
+      json = OpenURI.open_uri(@base_uri + path).read
+      return JSON.parse(json, JSON_OPTS)
+    end
+  end
+end
+
+GF = GrowthForecast # alias
