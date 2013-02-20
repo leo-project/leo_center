e5f8279e6365228ffebe7e432913657d0412d453
Glass_saga <glass.saga@gmail.com>
2013-02-20 15:24:27 +0900

    move SI formatter to util.js

--- public/js/nodes.js
+++ public/js/nodes.js
@@ -4,21 +4,6 @@
     fields: ["type", "node", "status", "ring_hash_current", "ring_hash_previous", "joined_at"]
   });
 
-  Ext.util.Format.SI = function(number, format) {
-    var format = format || "0";
-    if (number < 1024) {
-        return Ext.util.Format.number(number, format);
-    } else if (number < 1048576) {
-        return Ext.util.Format.number(Math.round(((number * 100) / 1024)) / 100, format) + "K";
-    } else if (number < 1073741824) {
-        return Ext.util.Format.number(Math.round(((number * 100) / 1048576)) / 100, format) + "M";
-    } else if (number < 1099511627776) {
-        return Ext.util.Format.number(Math.round(((number * 100) / 1073741824)) / 100, format) + "G";
-    } else {
-        return Ext.util.Format.number(Math.round(((number * 100) / 1099511627776)) / 100, format) + "T";
-    }
-  };
-
   Ext.define("LeoTamer.SNMP.Chart", {
     extend: "Ext.panel.Panel",
 
--- public/js/util.js
+++ public/js/util.js
@@ -38,4 +38,19 @@
       if (btn === "ok") callback(password);
     });
   }
+
+  Ext.util.Format.SI = function(number, format) {
+    var format = format || "0";
+    if (number < 1024) {
+        return Ext.util.Format.number(number, format);
+    } else if (number < 1048576) {
+        return Ext.util.Format.number(Math.round(((number * 100) / 1024)) / 100, format) + "K";
+    } else if (number < 1073741824) {
+        return Ext.util.Format.number(Math.round(((number * 100) / 1048576)) / 100, format) + "M";
+    } else if (number < 1099511627776) {
+        return Ext.util.Format.number(Math.round(((number * 100) / 1073741824)) / 100, format) + "G";
+    } else {
+        return Ext.util.Format.number(Math.round(((number * 100) / 1099511627776)) / 100, format) + "T";
+    }
+  };
 }).call(this);
