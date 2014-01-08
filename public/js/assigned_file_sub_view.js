333002f29827e30e34828eb81023735d35daaec5
Yosuke Hara <yousuke.hara@gmail.com>
2014-01-08 16:36:03 +0900

    Polish up the views#4

--- public/js/assigned_file_sub_view.js
+++ public/js/assigned_file_sub_view.js
@@ -19,166 +19,172 @@
 // under the License.
 //
 //======================================================================
+// @doc SUB VIEW - Assigned File in the AdminView
+//
 (function() {
-  Ext.define("LeoCenter.model.AssignedFile", {
-    extend: "Ext.data.Model",
-    fields: [
-      "node", "vnode_id", "size", "clock", "checksum",
-      { name: "timestamp", type: "date", dateFormat: "U" },
-      "delete", "num_of_chunks"
-    ]
-  });
+  Ext.define(
+    "LeoCenter.model.AssignedFile",
+    { extend: "Ext.data.Model",
+      fields: [ "node",
+                "vnode_id",
+                "size",
+                "clock",
+                "checksum",
+                { name: "timestamp", type: "date", dateFormat: "U" },
+                "delete", "num_of_chunks"
+              ]
+    });
 
-  Ext.define(PANE_SUB_ASSIGNED_FILE, {
-    extend: "Ext.panel.Panel",
-    id: "assigned_file_sub_view",
-    title: "Assigned File",
-    layout: "border",
-    border: false,
+  Ext.define(
+    PANE_SUB_ASSIGNED_FILE,
+    { extend: "Ext.panel.Panel",
+      id: "assigned_file_sub_view",
+      title: "Assigned File",
+      layout: "border",
+      border: false,
 
-    load: function() {
-      this.store.load();
-    },
+      store: Ext.create(
+        "Ext.data.Store",
+        { model: "LeoCenter.model.AssignedFile",
+          proxy: Ext.create("LeoCenter.proxy.Ajax.noParams", {
+            url: "whereis/list.json"
+          })
+        }),
 
-    store: Ext.create("Ext.data.Store", {
-      model: "LeoCenter.model.AssignedFile",
-      proxy: Ext.create("LeoCenter.proxy.Ajax.noParams", {
-        url: "whereis/list.json"
-      })
-    }),
+      detail_store: Ext.create(
+        "Ext.data.ArrayStore",
+        { fields: ["name", "value"],
+          data: []
+        }),
 
-    detail_store: Ext.create("Ext.data.ArrayStore", {
-      fields: ["name", "value"],
-      data: []
-    }),
+      initComponent: function() {
+        var self = this;
 
-    initComponent: function() {
-      var self = this;
+        self.grid = Ext.create(
+          "Ext.grid.Panel",
+          { region: "center",
+            border: false,
+            forceFit: true,
+            store: self.store,
+            tbar: [{ xtype: "textfield",
+                     id: "whereis_grid_tbar_path",
+                     fieldLabel: "<img src='images/filter.png'> Path:",
+                     labelWidth: 60,
+                     width: 500,
 
-      self.grid = Ext.create("Ext.grid.Panel", {
-        region: "center",
-        border: false,
-        forceFit: true,
-        store: self.store,
-        tbar: [{
-          xtype: "textfield",
-          id: "whereis_grid_tbar_path",
-          fieldLabel: "<img src='images/filter.png'> Path:",
-          labelWidth: 60,
-          width: 500,
-          listeners: {
-            change: function() {
-              delete self.path;
-              self.store.removeAll();
-              self.detail_store.removeAll();
-              self.detail_grid.setTitle("");
-            },
-            specialkey: function(text_field, event) {
-              if (event.getKey() !== event.ENTER) return;
-              var path = text_field.getValue();
-              if (path === "") return;
-              self.path = path;
-              self.store.load({
-                params: {
-                  path: path
-                },
-                callback: function() {
-                  var grid = self.grid;
-                  grid.getSelectionModel().select(0);
-                  grid.getView().focus(null, 500);
-                }
-              });
-            }
-          }
-        },
-               "->",
-               {
-                 icon: "images/reload.png",
-                 handler: function() {
-                   if (!self.path) return;
-                   self.store.load({
-                     params: {
-                       path: self.path
-                     },
-                     callback: function() {
-                       self.grid.getSelectionModel().select(0);
+                     listeners: {
+                       change: function() {
+                         delete self.path;
+                         self.store.removeAll();
+                         self.detail_store.removeAll();
+                         self.detail_grid.setTitle("");
+                       },
+                       specialkey: function(text_field, event) {
+                         if (event.getKey() !== event.ENTER) return;
+                         var path = text_field.getValue();
+                         if (path === "") return;
+                         self.path = path;
+                         self.store.load({
+                           params: {
+                             path: path
+                           },
+                           callback: function() {
+                             var grid = self.grid;
+                             grid.getSelectionModel().select(0);
+                             grid.getView().focus(null, 500);
+                           }
+                         });
+                       }
                      }
-                   });
-                 }
-               }],
-        columns: {
-          defaults: { resizable: false },
-          items: [
-            {
-              dataIndex: "delete",
-              width: 8,
-              renderer: function(value) {
-                if (value === false) return "";
-                return "<img src='images/trash.png'>";
+                   },
+                   "->",
+                   {
+                     icon: "images/reload.png",
+                     handler: function() {
+                       if (!self.path) return;
+                       self.store.load({
+                         params: {
+                           path: self.path
+                         },
+                         callback: function() {
+                           self.grid.getSelectionModel().select(0);
+                         }
+                       });
+                     }
+                   }],
+
+            columns: { defaults: { resizable: false },
+                       items: [{ dataIndex: "delete",
+                                 width: 8,
+                                 renderer: function(value) {
+                                   if (value === false) return "";
+                                   return "<img src='images/trash.png'>";
+                                 }
+                               },
+                               { header: "Node", dataIndex: "node" },
+                               {
+                                 header: "Size",
+                                 dataIndex: "size",
+                                 width: 30,
+                                 renderer: function(size) {
+                                   var format_string = "0";
+                                   if (size < 1024) {
+                                     return Ext.util.Format.number(size, format_string);
+                                   } else if (size < 1048576) {
+                                     return Ext.util.Format.number(
+                                       Math.round(((size * 100) / 1024)) / 100, format_string) + " KB";
+                                   } else if (size < 1073741824) {
+                                     return Ext.util.Format.number(
+                                       Math.round(((size * 100) / 1048576)) / 100, format_string) + " MB";
+                                   } else if (size < 1099511627776) {
+                                     return Ext.util.Format.number(
+                                       Math.round(((size * 100) / 1073741824)) / 100, format_string) + " GB";
+                                   } else {
+                                     return Ext.util.Format.number(
+                                       Math.round(((size * 100) / 1099511627776)) / 100, format_string) + " TB";
+                                   }
+                                 }
+                               },
+                               { header: "Actual Size", dataIndex: "size", width: 40 },
+                               { header: "Timestamp",
+                                 dataIndex: "timestamp",
+                                 renderer: Ext.util.Format.dateRenderer("c")
+                               }
+                              ]
+                     },
+            listeners: {
+              select: function(grid, record, index) {
+                self.detail_grid.setTitle(record.get("node"));
+                self.detail_store.loadData(
+                  [ ["Ring address", record.get("vnode_id")],
+                    ["Clock", record.get("clock")],
+                    ["Number of chunks", record.get("num_of_chunks")],
+                    ["Checksum", record.get("checksum")]
+                  ]);
               }
-            },
-            { header: "Node", dataIndex: "node" },
-            {
-              header: "Size",
-              dataIndex: "size",
-              width: 30,
-              renderer: function(size) {
-                var format_string = "0";
-                if (size < 1024) {
-                  return Ext.util.Format.number(size, format_string);
-                } else if (size < 1048576) {
-                  return Ext.util.Format.number(Math.round(((size * 100) / 1024)) / 100, format_string) + " KB";
-                } else if (size < 1073741824) {
-                  return Ext.util.Format.number(Math.round(((size * 100) / 1048576)) / 100, format_string) + " MB";
-                } else if (size < 1099511627776) {
-                  return Ext.util.Format.number(Math.round(((size * 100) / 1073741824)) / 100, format_string) + " GB";
-                } else {
-                  return Ext.util.Format.number(Math.round(((size * 100) / 1099511627776)) / 100, format_string) + " TB";
-                }
-              }
-            },
-            { header: "Actual Size", dataIndex: "size", width: 40 },
-            {
-              header: "Timestamp",
-              dataIndex: "timestamp",
-              renderer: Ext.util.Format.dateRenderer("c")
             }
-          ]
-        },
-        listeners: {
-          select: function(grid, record, index) {
-            self.detail_grid.setTitle(record.get("node"));
-            self.detail_store.loadData([
-              ["Ring address", record.get("vnode_id")],
-              ["Clock", record.get("clock")],
-              ["Number of chunks", record.get("num_of_chunks")],
-              ["Checksum", record.get("checksum")]
-            ]);
-          }
-        }
-      });
+          });
 
-      self.detail_grid = Ext.create("Ext.grid.Panel", {
-        region: "east",
-        title: "&nbsp;", // force blank title
-        width: 400,
-        forceFit: true,
-        store: self.detail_store,
-        hideHeaders: true,
-        columns: {
-          defaults: { resizable: false },
-          items: [
-            { dataIndex: "name", width: 60 },
-            { dataIndex: "value" }
-          ]
-        }
-      });
+        self.detail_grid = Ext.create(
+          "Ext.grid.Panel",
+          { region: "east",
+            title: "&nbsp;",
+            width: 400,
+            forceFit: true,
+            store: self.detail_store,
+            hideHeaders: true,
+            columns: {
+              defaults: { resizable: false },
+              items: [{ dataIndex: "name", width: 60 },
+                      { dataIndex: "value" }
+                     ]
+            }
+          });
 
-      Ext.apply(self, {
-        items: [self.grid, self.detail_grid]
-      });
-
-      return self.callParent(arguments);
-    }
-  });
+        Ext.apply(self, {
+          items: [self.grid, self.detail_grid]
+        });
+        return self.callParent(arguments);
+      }
+    });
 }).call(this);
--- public/js/endpoint_sub_view.js
+++ public/js/endpoint_sub_view.js
@@ -19,139 +19,136 @@
 // under the License.
 //
 //======================================================================
+// @doc SUB VIEW - Endpoints in the AdminView
+//
 (function() {
-  Ext.define("LeoCenter.model.Endpoints", {
-    extend: "Ext.data.Model",
-    fields: [
-      "endpoint",
-      { name: "created_at", type: "date", dateFormat: "U" }
-    ]
-  });
+  Ext.define("LeoCenter.model.Endpoints",
+             { extend: "Ext.data.Model",
+               fields: [
+                 "endpoint",
+                 { name: "created_at", type: "date", dateFormat: "U" }
+               ]
+             });
 
-  Ext.define(PANE_SUB_ENDPOINTS, {
-    extend: "Ext.panel.Panel",
-    id: "endpoint_sub_view",
-    title: "Endpoints",
-    layout: "border",
-    border: false,
+  Ext.define(
+    PANE_SUB_ENDPOINTS,
+    { extend: "Ext.panel.Panel",
+      id: "endpoint_sub_view",
+      title: "Endpoints",
+      layout: "border",
+      border: false,
 
-    listeners: {
-      activate: function(self) {
-        self.load();
-      }
-    },
-
-    load: function() {
-      this.store.load();
-    },
-
-    store: Ext.create("Ext.data.Store", {
-      model: "LeoCenter.model.Endpoints",
-      proxy: Ext.create("LeoCenter.proxy.Ajax.noParams", {
-        url: "endpoints/list.json"
-      })
-    }),
-
-    add_endpoint: function(self) {
-      var title = "Add New Endpoint";
-      var msg = "Please input endpoint name";
-
-      Ext.Msg.prompt(title, msg, function(btn, value) {
-        if (btn == "ok") {
-          Ext.Ajax.request({
-            url: "endpoints/add_endpoint",
-            method: "POST",
-            params: { endpoint: value },
-            success: function(response) {
-              self.load();
-            },
-            failure: function(response) {
-              LeoCenter.Msg.alert("Error!", response.responseText);
-            }
-          })
+      listeners: {
+        activate: function(self) {
+          self.store.load();
         }
-      })
-    },
+      },
 
-    delete_endpoint: function(self) {
-      var self = this;
-      var title = "Delete Endpoint";
-      var last_selected = self.grid.getSelectionModel().getLastSelected();
-      if (!last_selected) {
-        LeoCenter.Msg.alert("Error!", "Please select a endpoint.");
-      }
-      else {
-        var endpoint = last_selected.data.endpoint;
-        var msg = "Are you sure to delete endpoint: '" + endpoint + "'?";
-        Ext.Msg.on("beforeshow",  function (win) {
-          win.defaultFocus = 2; // set default focus to "No" button
-        });
-        Ext.Msg.confirm(title, msg, function(btn) {
-          if (btn == "yes") {
+      store: Ext.create(
+        "Ext.data.Store",
+        { model: "LeoCenter.model.Endpoints",
+          proxy: Ext.create("LeoCenter.proxy.Ajax.noParams", {
+            url: "endpoints/list.json"
+          })
+        }),
+
+      add_endpoint: function(self) {
+        var title = "Add New Endpoint";
+        var msg = "Please input endpoint name";
+
+        Ext.Msg.prompt(title, msg, function(btn, value) {
+          if (btn == "ok") {
             Ext.Ajax.request({
-              url: "endpoints/delete_endpoint",
-              method: "DELETE",
-              params: { endpoint: endpoint },
-              success: function(response, opts) {
-                self.load();
+              url: "endpoints/add_endpoint",
+              method: "POST",
+              params: { endpoint: value },
+              success: function(response) {
+                self.store.load();
               },
-              failure: function(response, opts) {
+              failure: function(response) {
                 LeoCenter.Msg.alert("Error!", response.responseText);
               }
             })
           }
         })
-      }
-    },
+      },
 
-    initComponent: function() {
-      var self = this;
-
-      self.grid = Ext.create("Ext.grid.Panel", {
-        region: "center",
-        border: false,
-        forceFit: true,
-        store: self.store,
-        tbar: [{
-          text: "Add Endpoint",
-          icon: "images/add.png",
-          handler: function() {
-            self.add_endpoint(self);
-          }
-        }, {
-          text: "Delete Endpoint",
-          icon: "images/remove.png",
-          handler: function() {
-            self.delete_endpoint(self);
-          }
-        },
-               "->",
-               {
-                 icon: "images/reload.png",
-                 handler: function() {
-                   self.load();
-                 }
-               }],
-        columns: {
-          defaults: { resizable: false },
-          items: [{
-            header: "Endpoint",
-            dataIndex: "endpoint",
-            width: 30,
-            renderer: Ext.htmlEncode,
-          }, {
-            header: "Created at",
-            dataIndex: "created_at",
-            renderer: Ext.util.Format.dateRenderer("c")
-          }]
+      delete_endpoint: function(self) {
+        var self = this;
+        var title = "Delete Endpoint";
+        var last_selected = self.grid.getSelectionModel().getLastSelected();
+        if (!last_selected) {
+          LeoCenter.Msg.alert("Error!", "Please select a endpoint.");
         }
-      });
+        else {
+          var endpoint = last_selected.data.endpoint;
+          var msg = "Are you sure to delete endpoint: '" + endpoint + "'?";
+          Ext.Msg.on("beforeshow",  function (win) {
+            win.defaultFocus = 2; // set default focus to "No" button
+          });
+          Ext.Msg.confirm(title, msg, function(btn) {
+            if (btn == "yes") {
+              Ext.Ajax.request(
+                { url: "endpoints/delete_endpoint",
+                  method: "DELETE",
+                  params: { endpoint: endpoint },
+                  success: function(response, opts) {
+                    self.store.load();
+                  },
+                  failure: function(response, opts) {
+                    LeoCenter.Msg.alert("Error!", response.responseText);
+                  }
+                })
+            }
+          })
+        }
+      },
 
-      Ext.apply(self, {
-        items: self.grid
-      });
 
-      return self.callParent(arguments);
-    }
-  });
+      initComponent: function() {
+        var self = this;
+
+        self.grid = Ext.create(
+          "Ext.grid.Panel",
+          { region: "center",
+            border: false,
+            forceFit: true,
+            store: self.store,
+            tbar: [{ text: "Add Endpoint",
+                     icon: "images/add.png",
+                     handler: function() {
+                       self.add_endpoint(self);
+                     }
+                   },
+                   { text: "Delete Endpoint",
+                     icon: "images/remove.png",
+                     handler: function() {
+                       self.delete_endpoint(self);
+                     }
+                   },
+                   "->",
+                   { icon: "images/reload.png",
+                     handler: function() {
+                       self.store.load();
+                     }
+                   }],
+            columns: { defaults: { resizable: false },
+                       items: [{ header: "Endpoint",
+                                 dataIndex: "endpoint",
+                                 width: 30,
+                                 renderer: Ext.htmlEncode,
+                               },
+                               { header: "Created at",
+                                 dataIndex: "created_at",
+                                 renderer: Ext.util.Format.dateRenderer("c")
+                               }]
+                     }
+          });
+
+        Ext.apply(self, {
+          items: self.grid
+        });
+        return self.callParent(arguments);
+      }
+    });
 }).call(this);
